0ec98eb4f20ae6d119cc65ce9aca4cbc2f845fae
Hossam Samy <hossamsamy234@gmail.com>
2025-03-25 04:10:57 +0200

    finish global exception handler

---
+++ SnapSell.Application/Exceptions/BaseException.cs
@@ -0,0 +1,8 @@
+﻿namespace SnapSell.Application.Exceptions
+{
+    public class BaseException: Exception
+    {
+        public BaseException(IEnumerable<ValidationError> errors) => Errors = errors;
+        public IEnumerable<ValidationError> Errors { get; }
+    }
+}
