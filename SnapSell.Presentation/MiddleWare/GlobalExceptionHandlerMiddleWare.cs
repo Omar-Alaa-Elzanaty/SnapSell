dcee953a8db1fa64c358bd7d86c745246c1a56f2
Hossam Samy <hossamsamy234@gmail.com>
2025-03-28 10:16:04 +0200

    edit global error handler

--- SnapSell.Presentation/MiddleWare/GlobalExceptionHandlerMiddleWare.cs
+++ SnapSell.Presentation/MiddleWare/GlobalExceptionHandlerMiddleWare.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation.Results;
 using Mapster;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using SnapSell.Application.Exceptions;
 using SnapSell.Domain.Dtos.ResultDtos;
 using SnapSell.Domain.Extnesions;
@@ -24,18 +25,41 @@ namespace SnapSell.Presentation.MiddleWare
             }
             catch (BaseException ex)
             {
+                await HandleBaseExceptionAsync(context, ex);
+            }
+            catch (Exception ex)
+            {
                 await HandleExceptionAsync(context, ex);
             }
+
         }
 
        
-        private static async Task HandleExceptionAsync(HttpContext context, BaseException ex)
+        private static async Task HandleBaseExceptionAsync(HttpContext context, BaseException ex)
         {
+            
             var response = new Result<object>
             {
                 Message = ex.Message,
                 StatusCode = (HttpStatusCode)context.Response.StatusCode,
                 Errors = ValidationExtenstion.GetErrorsDictionary(ex.Errors.Adapt<List<ValidationFailure>>())
+            };
+
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var exceptionResult = JsonSerializer.Serialize(response, jsonOptions);
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(exceptionResult);
+        }
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            
+            var response = new Result<object>
+            {
+                Message = ex.Message,
+                StatusCode = (HttpStatusCode)context.Response.StatusCode,
             };
 
             var jsonOptions = new JsonSerializerOptions()
