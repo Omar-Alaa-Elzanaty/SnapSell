a3799a9944964816e48d04982575c3556c6ab2ea
omar alaa <omaralaaelzanaty@gmail.com>
2025-05-22 18:55:44 +0300

    Add ApproveBendingStoreCommand and its validator
    Introduces the `ApproveBendingStoreCommand` implementing the `IRequest<Result<string>>` interface from MediatR, with a `StoreId` property. A handler class `ApproveBendingStoreCommandHandler` is defined but not yet implemented. Additionally, a validator class `ApproveBendingStoreCommandValidator` is added to validate the command, though no validation rules are currently specified.

---
+++ SnapSell.Application/Features/Admins/Commands/ApproveBendingStore/ApproveBendingStoreCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SnapSell.Domain.Dtos.ResultDtos;
+
+namespace SnapSell.Application.Features.Admins.Commands.ApproveBendingStore
+{
+    public sealed record ApproveBendingStoreCommand : IRequest<Result<string>>
+    {
+        public string StoreId { get; set; }
+    }
+
+    internal class ApproveBendingStoreCommandHandler : IRequestHandler<ApproveBendingStoreCommand, Result<string>>
+    {
+        public Task<Result<string>> Handle(ApproveBendingStoreCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SnapSell.Application/Features/Admins/Commands/ApproveBendingStore/ApproveBendingStoreCommand.csValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace SnapSell.Application.Features.Admins.Commands.ApproveBendingStore
+{
+    public class ApproveBendingStoreCommandValidator:AbstractValidator<ApproveBendingStoreCommand>
+    {
+        public ApproveBendingStoreCommandValidator()
+        {
+
+        }
+    }
+}
